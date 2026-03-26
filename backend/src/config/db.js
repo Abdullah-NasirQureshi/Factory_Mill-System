d9204ee74413ac04c3710a1919168b5b2b4bddc7
Abdullah-NasirQureshi <qureshiabdullah038@gmail.com>
2026-03-27 02:44:40 +0500

    Minor changes in db file, for cloud db and before test deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 
 const pool = new Pool({
   host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT) || 5400,
+  port:     parseInt(process.env.DB_PORT) || 5432,
   user:     process.env.DB_USER     || 'postgres',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'factory_erp',
+  database: process.env.DB_NAME     || 'neondb',
+  ssl:      { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
 });
